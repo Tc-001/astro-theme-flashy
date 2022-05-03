4920701d76127103169e45f582a56996bc47afc7
tc001 <tc001@duck.com>
2022-05-03 16:28:50 +0300

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import nodejs from '@astrojs/node';
 
+// https://astro.build/config
 export default defineConfig({
-  adapter: nodejs()
-})
+  adapter: nodejs(),
+  integrations: []
+});
