1955cac47bb2fbcb2a710fdcbf34783ddd854dd1
tc001 <tc001@duck.com>
2022-05-03 16:48:29 +0300

    Do not hardcode nav

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+
+export const nav = [
+	{
+		title: "Home", 
+		url: "/", 
+		color: "#0450b4",
+		icon: "bi:house"
+	},
+	{
+		title: "Blog", 
+		url: "/blog", 
+		color: "#b4418e",
+		icon: "bi:newspaper"
+	},
+	{
+		title: "About me", 
+		url: "/about", 
+		color: "#ea515f", 
+		icon: "bi:info-square"
+	},
+]
